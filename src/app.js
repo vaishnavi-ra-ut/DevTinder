89a10b1853d7b84507cc5092c468c6a67393b95c
Vaishnavi Raut <rautvaishnavi29@gmail.com>
2025-05-31 01:42:02 +0530

    Done with Post API

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 const express = require("express");
-
+const connectDB = require("./config/Database");
 const app = express();
+const User = require("./models/User")
 
-app.use((req, res) => {
-    res.send("Helloooo!");
+app.use(express.json()); 
+
+app.post("/signup", async (req , res) => {
+    const user = new User(req.body);
+
+    await user.save();
+    res.send("User added Succesfully!");;
 })
 
-app.listen(3000 , ()=>{
-    console.log("Server is litening on port 3000...")
-});
+connectDB()
+  .then(() => {
+    console.log("Database connection established...");
+
+    app.listen(3000, () => {
+      console.log("Server is successfully listening on port 3000...");
+    });
+  })
+  .catch((err) => {
+    console.error("Database cannot be connected!!", err);
+  });
